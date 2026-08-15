2643fda5ad71aac595d801634b5663de2871c06d
ghugo6403 <ghugo6403@gmail.com>
2026-08-15 16:57:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,7 @@ import streamlit as st
 from config import LEAGUES, VALUE_BET_EDGE_THRESHOLD, CURRENT_SEASON
 
 import mock_data
-from football_data import FootballDataClient, cached_past_matches,
-cached_upcoming_matches
+from football_data import FootballDataClient, cached_past_matches,cached_upcoming_matches
 from data_processing import build_team_history, merge_fixtures_with_odds
 from odds_api import OddsAPIClient, cached_odds
 from poisson_model import DixonColesModel
